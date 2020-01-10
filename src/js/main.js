eac8efdd8108d1a5dc134d1be4dc0f24503fdfa3
Mariusz Rukowicz <mariuszrukowicz@wp.pl>
2020-01-10 21:12:07 +0100

    feat: fixed gallery photos path

--- src/js/main.js
+++ src/js/main.js
@@ -347,3 +347,25 @@ jQuery(document).ready(function() {
 	});
 
 });
+(function($) {
+
+	// Menu filter
+	$("#menu-filters li a").click(function() {
+	  $("#menu-filters li a").removeClass('active');
+	  $(this).addClass('active');
+  
+	  var selectedFilter = $(this).data("filter");
+	  //  $("#menu-wrapper").fadeTo(100, 0);
+  
+	  $(".menu-restaurant").fadeOut();
+  
+	  setTimeout(function() {
+		$(selectedFilter).slideDown();
+		//$("#menu-wrapper").fadeTo(300, 1);
+	  }, 300);
+	});
+  
+	})(jQuery);
+  
+   
+  
